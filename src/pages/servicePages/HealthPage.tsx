723f810f186f53601281d8b3bcc04efefd5800a9
Elizaveta <elizaveta.muravyeva87@gmail.com>
2026-01-04 21:03:25 +0200

    FE:some changes

--- src/pages/servicePages/HealthPage.tsx
+++ src/pages/servicePages/HealthPage.tsx
@@ -1,9 +1,25 @@
+import {fetchHealth} from "../../api/client.ts";
+import {useState} from "react";
 
 
 const HealthPage = () => {
+
+    const [status, setStatus] = useState<string>("");
+
+    const healthCheck = async () => {
+        try{
+            const data = await fetchHealth();
+            setStatus(JSON.stringify(data, null, 2));
+        }catch(e){
+            setStatus("Error, check console");
+        }
+    }
+
     return (
         <div>
             <h1>Health Page</h1>
+            <button onClick={healthCheck}>Check health</button>
+            <p>The status of health is: <h3>{status}</h3></p>
         </div>
     );
 };
