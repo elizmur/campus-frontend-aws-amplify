305c564194ccd1ee3fc2fad492f6dc1c0ea10d83
Elizaveta <elizaveta.muravyeva87@gmail.com>
2026-01-13 22:33:18 +0200

    commeted proxy for working on localhost

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        proxy: {
-            "/health": "http://localhost:8080",
-            "/auth/login": "http://localhost:8080",
-            "/auth/me": "http://localhost:8080",
-            "/auth/refresh": "http://localhost:8080",
-
-        },
-    },
+    // server: {
+    //     proxy: {
+    //         "/health": "http://localhost:8080",
+    //         "/auth/login": "http://localhost:8080",
+    //         "/auth/me": "http://localhost:8080",
+    //         "/auth/refresh": "http://localhost:8080",
+    //
+    //     },
+    // },
 });
 
